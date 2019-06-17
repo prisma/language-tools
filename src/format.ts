b9e7d8b7350eb3d0296d3cf5ef398a3b44f8f17b
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2019-06-17 20:34:04 +0200

    Added linter.

--- src/format.ts
+++ src/format.ts
@@ -1,34 +1,10 @@
-import { spawn } from 'child_process'
+import exec from './exec'
 
-export default function format(exec_path: string, ident_width: number, text: string): Promise<string> {
-  const fmt = spawn(exec_path, ['-s', ident_width.toString()])
-
-  const chunks = []
-
-  fmt.stdout.on('data', (data) => {
-    chunks.push(data.toString())
-  });
-
-  const err_chunks = []
-
-  fmt.stderr.on('data', (data) => {
-    err_chunks.push(data.toString())
-  });
-
-
-  fmt.stdin.setDefaultEncoding('utf-8');
-  fmt.stdin.write(text);
-  fmt.stdin.end();
-
-  return new Promise((resolve, reject) => {
-    fmt.on('exit', (code) => {
-      if(code === 0 && err_chunks.length === 0) {
-        resolve(chunks.join(""))
-      } else {
-        console.log("prisma-fmt error'd during formatting.");
-        console.log(err_chunks);
-        resolve(text);
-      }
-    })
-  })
+export default async function format(exec_path: string, ident_width: number, text: string): Promise<string> {
+  try {
+    return await exec(exec_path, ['-s', ident_width.toString()], text)
+  } catch(errors) {
+    console.warn('prisma-fmt error\'d during formatting. This was likely due to a syntax error. Please see linter output.');
+    return text;
+  }
 }
--- src/provider.ts
+++ src/provider.ts
@@ -9,7 +9,7 @@ import {
 
 import format from'./format'
 
-function fullDocumentRange(document: TextDocument): Range {
+export function fullDocumentRange(document: TextDocument): Range {
   const lastLineId = document.lineCount - 1;
   return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
 }
