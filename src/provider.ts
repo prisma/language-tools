3174a0f8f74729cfc3f5b0e97f35eb87800a6ba7
Matthew Mueller <mattmuelle@gmail.com>
2019-12-02 15:20:25 +0100

    add formatting

--- src/provider.ts
+++ src/provider.ts
@@ -5,31 +5,35 @@ import {
   FormattingOptions,
   CancellationToken,
   TextEdit,
-} from 'vscode';
+} from 'vscode'
 
-import format from'./format'
+import format from './format'
 
 export function fullDocumentRange(document: TextDocument): Range {
-  const lastLineId = document.lineCount - 1;
-  return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+  const lastLineId = document.lineCount - 1
+  return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length)
 }
 
 class PrismaEditProvider implements DocumentFormattingEditProvider {
-    private exec_path: string
+  private exec_path: string
 
-    constructor(exec_path) {
-      this.exec_path = exec_path;
-    }
+  constructor(exec_path) {
+    this.exec_path = exec_path
+  }
 
-    provideDocumentFormattingEdits(
-        document: TextDocument,
-        options: FormattingOptions,
-        token: CancellationToken
-    ): Promise<TextEdit[]> {
-      return format(this.exec_path, options.tabSize, document.getText()).then((formatted) => 
-        [TextEdit.replace(fullDocumentRange(document), formatted)]
-      )
-    }
+  provideDocumentFormattingEdits(
+    document: TextDocument,
+    options: FormattingOptions,
+    token: CancellationToken,
+  ): Promise<TextEdit[]> {
+    return format(
+      this.exec_path,
+      options.tabSize,
+      document.getText(),
+    ).then(formatted => [
+      TextEdit.replace(fullDocumentRange(document), formatted),
+    ])
+  }
 }
 
-export default PrismaEditProvider;
+export default PrismaEditProvider
