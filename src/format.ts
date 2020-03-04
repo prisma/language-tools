daa57765b060f3fb4a8e6105b747859f40eabb44
Julius de Bruijn <bruijn@prisma.io>
2020-03-04 13:55:14 +0000

    Changing how prisma-fmt takes parameters (#51)

--- src/format.ts
+++ src/format.ts
@@ -6,7 +6,7 @@ export default async function format(
   text: string,
 ): Promise<string> {
   try {
-    return await exec(exec_path, ['-s', ident_width.toString()], text)
+    return await exec(exec_path, ['format', '-s', ident_width.toString()], text)
   } catch (errors) {
     console.warn(
       "prisma-fmt error'd during formatting. This was likely due to a syntax error. Please see linter output.",
