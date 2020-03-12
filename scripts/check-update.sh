1a64741bcc66a71f810dcfb07270ae99d2c899ec
Divyendu Singh <singh@prisma.io>
2020-03-12 18:38:44 +0530

    feat(automate-release): commit the prisma_version

--- scripts/check-update.sh
+++ scripts/check-update.sh
@@ -11,6 +11,8 @@ echo "NPM_VERSION: $NPM_VERSION"
 if [ "$CURRENT_VERSION" != "$NPM_VERSION" ]; then
     echo "UPDATING to $NPM_VERSION"
     echo "$NPM_VERSION" > scripts/prisma_version
+    git add -A .
+    git commit -m "bump prisma_version to $NPM_VERSION"
     yarn run vsce:publish
 else
     echo "CURRENT_VERSION ($CURRENT_VERSION) and NPM_VERSION ($NPM_VERSION) are same"
