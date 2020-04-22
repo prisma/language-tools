5597c5b8183a070630991e332db643dbdd712809
Carmen Berndt <carmen@berndt-home.de>
2020-04-22 10:03:59 +0200

    add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  overrides: [
+    // {
+    //   files: ['*.js'],
+    //   rules: {},
+    // },
+    {
+      files: ['*.ts'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier/@typescript-eslint',
+      ],
+      rules: {
+        // '@typescript-eslint/no-use-before-define': 'off',
+        // 'no-useless-escape': 'off',
+      },
+    },
+  ],
+}
