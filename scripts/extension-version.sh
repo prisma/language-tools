87596e5b60b6a96cbdd80fb9040d6d1682138916
Divyendu Singh <singh@prisma.io>
2020-04-28 15:13:56 +0530

    feat(patch): use patch versioning (#115)
    Co-Authored-By: Jan Piotrowski <piotrowski+github@gmail.com>

--- scripts/extension-version.sh
+++ scripts/extension-version.sh
@@ -3,9 +3,16 @@
 set -eu
 
 CHANNEL="$1"
+NEXT="$2"
 
 if [ "$CHANNEL" = "dev" ]; then
-    vsce show Prisma.prisma-dev --json | jq ".versions[0].version" | tr -d '"'
+    EXTENSION_VERSION=$(vsce show Prisma.prisma-dev --json | jq ".versions[0].version" | tr -d '"')
 else
-    vsce show Prisma.prisma --json | jq ".versions[0].version" | tr -d '"'
+    EXTENSION_VERSION=$(vsce show Prisma.prisma --json | jq ".versions[0].version" | tr -d '"')
+fi
+
+if [ "$NEXT" = "patch" ]; then
+    node -e "const semver = require('semver'); console.log(semver.inc('$EXTENSION_VERSION', 'patch'))"
+else
+    echo "$EXTENSION_VERSION"
 fi
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,6 +22,8 @@ echo "CHANNEL: $CHANNEL"
 PRISMA_VERSION=$2
 echo "PRISMA_VERSION: $PRISMA_VERSION"
 
+NEXT_EXTENSION_VERSION=$3
+echo "NEXT_EXTENSION_VERSION: $NEXT_EXTENSION_VERSION"
 
 # Try to publish if $AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN (Personal Access Token - https://code.visualstudio.com/api/working-with-extensions/publishing-extension#get-a-personal-access-token) exists 
 if [ -z "$AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN" ]; then
@@ -29,19 +31,29 @@ if [ -z "$AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN" ]; then
 elif [ -n "$AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN" ]; then
     if [ "$PRODUCTION" = "1" ]; then
         echo "Publishing $CHANNEL release"
-        ./node_modules/.bin/vsce publish "$PRISMA_VERSION" --pat "$AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN"
+        ./node_modules/.bin/vsce publish "$NEXT_EXTENSION_VERSION" --pat "$AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN"
     else
         echo "Printing the command because PRODUCTION is not set"
         echo "sh ./scripts/bump.sh" # The actual execution of this command is in check-update.sh becuase git working tree must be clean before calling `vsce publish`
-        echo "./node_modules/.bin/vsce publish \"$PRISMA_VERSION\" --pat $AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN"
+        echo "./node_modules/.bin/vsce publish \"$NEXT_EXTENSION_VERSION\" --pat $AZURE_DEVOPS_PERSONAL_ACCESS_TOKEN"
     fi
 fi
 
-# Sync with remote branch (only master, see publish.yml) when on dev channel, and push to it
 if [ "$PRODUCTION" = "1" ] && [ "$CHANNEL" = "dev" ]; then
     echo "Sync with ${GITHUB_REF} and push to it"
     git pull github "${GITHUB_REF}" --ff-only
     git push github HEAD:"${GITHUB_REF}"
-else
+elif [ "$PRODUCTION" = "1" ] && [ "$CHANNEL" = "latest" ]; then
+    echo "Sync with ${GITHUB_REF} and push to it"
+    git pull github "${GITHUB_REF}" --ff-only
+
+    # In the stable channel, we just need to commit the prisma_version_stable file
+    # to be able to track the Prisma version against which the current stable channel extension was published
+    git reset origin/master
+    git add ./scripts/prisma_version_stable
+    git commit -m "bump prisma_version to $NPM_VERSION"
+
+    git push github HEAD:"${GITHUB_REF}"
+else 
     echo "Not pushing because PRODUCTION is not set"
 fi
