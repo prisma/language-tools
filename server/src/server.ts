97604b41a49beae8f91574cb3a576ff66f40e4bd
Carmen Berndt <carmen@berndt-home.de>
2020-05-04 10:14:55 +0200

    code cleanup

--- server/src/server.ts
+++ server/src/server.ts
@@ -74,7 +74,10 @@ connection.onInitialize(async (params: InitializeParams) => {
       connection.console.error('Cannot install prisma-fmt: ' + err)
     }
   }
-  connection.console.info('Installed version of Prisma plugin prisma-fmt: ' + await util.getVersion())
+  connection.console.info(
+    'Installed version of Prisma plugin prisma-fmt: ' +
+      (await util.getVersion()),
+  )
 
   const result: InitializeResult = {
     capabilities: {
