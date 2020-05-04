b0ce810f688f31c9821c6bb1162aa6825e000b90
Carmen Berndt <carmen@berndt-home.de>
2020-05-04 15:24:56 +0200

    fix path to e2e tests

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CODE_TESTS_PATH="$(pwd)/client/out/test"
+CODE_TESTS_PATH="$(pwd)/clients/vscode/out/test"
 export CODE_TESTS_PATH
 
-CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
+CODE_TESTS_WORKSPACE="$(pwd)/clients/vscode/testFixture"
 export CODE_TESTS_WORKSPACE
 
-node "$(pwd)/client/out/test/runTest"
+node "$(pwd)/clients/vscode/out/test/runTest"
