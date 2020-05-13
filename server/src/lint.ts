25fdb3b6b28a2d618e01fee15975624189eb5af9
Carmen Berndt <carmen@berndt-home.de>
2020-05-13 18:11:50 +0200

    add window notification to give user feedback for graphql files

--- server/src/lint.ts
+++ server/src/lint.ts
@@ -16,9 +16,18 @@ export default async function lint(
     return JSON.parse(result)
   } catch (errors) {
     const errorMessage = "prisma-fmt error'd during linting.\n"
+
     if (onError) {
-      onError(errorMessage + errors)
+      if ((errors as string) && errors.includes('ParsingError')) {
+        // recognized Prisma 1 schema
+        onError(
+          "This file is a GraphQL file and is not supported by the Prisma extension. Please install the GraphQL extension ('https://marketplace.visualstudio.com/items?itemName=Prisma.vscode-graphql') from the marketplace and change the file extension to '.graphql'.",
+        )
+      } else {
+        onError(errorMessage + errors)
+      }
     }
+
     console.error(errorMessage)
     console.error(errors)
     return []
