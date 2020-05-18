c8708e4d7f40fdabb94bcfc88c206a36ddbc99f9
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-05-18 18:10:00 +0200

    Recognize Prisma1 schemas and tell user to install the other extension and rename file (#138)
    * add window notification to give user feedback for graphql files
    
    * use better worded instruction
    
    * use markdown
    
    * remove unusded import
    
    * add check for prisma1 schema
    
    * fix minor english things

--- server/src/lint.ts
+++ server/src/lint.ts
@@ -16,9 +16,22 @@ export default async function lint(
     return JSON.parse(result)
   } catch (errors) {
     const errorMessage = "prisma-fmt error'd during linting.\n"
+
     if (onError) {
-      onError(errorMessage + errors)
+      if (
+        (errors as string) &&
+        errors.includes('ParsingError') &&
+        text.includes('type')
+      ) {
+        // recognized Prisma 1 schema
+        onError(
+          "You are currently viewing a Prisma 1 datamodel which is based on the GraphQL syntax. The current Prisma VSCode extension doesn't support this syntax. To get proper syntax highlighting for this file, please change the file extension to `.graphql` and download the [GraphQL VSCode extension](https://marketplace.visualstudio.com/items?itemName=Prisma.vscode-graphql). Learn more [here](https://pris.ly/prisma1-vscode).",
+        )
+      } else {
+        onError(errorMessage + errors)
+      }
     }
+
     console.error(errorMessage)
     console.error(errors)
     return []
