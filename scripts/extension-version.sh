1aa4fa3d8bb5b60b85ab996d235cfbcd50d045b5
Carmen Berndt <carmen@berndt-home.de>
2020-05-19 12:05:20 +0200

    call vsce package from nodemodules folder

--- scripts/extension-version.sh
+++ scripts/extension-version.sh
@@ -6,9 +6,9 @@ CHANNEL="$1"
 NEXT="$2"
 
 if [ "$CHANNEL" = "dev" ]; then
-    EXTENSION_VERSION=$(vsce show Prisma.prisma-dev --json | jq ".versions[0].version" | tr -d '"')
+    EXTENSION_VERSION=$(./clients/vscode/node_modules/.bin/vsce show Prisma.prisma-dev --json | jq ".versions[0].version" | tr -d '"')
 else
-    EXTENSION_VERSION=$(vsce show Prisma.prisma --json | jq ".versions[0].version" | tr -d '"')
+    EXTENSION_VERSION=$(./clients/vscode/node_modules/.bin/vsce show Prisma.prisma --json | jq ".versions[0].version" | tr -d '"')
 fi
 
 if [ "$NEXT" = "patch" ]; then
