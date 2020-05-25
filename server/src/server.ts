0f9808d7591c9a4b13776a132980bdec0070774c
Carmen Berndt <carmen@berndt-home.de>
2020-05-25 10:39:39 +0200

    rename output message

--- server/src/server.ts
+++ server/src/server.ts
@@ -81,7 +81,7 @@ connection.onInitialize(async (params: InitializeParams) => {
 
   const pj = require('../../package.json')
   connection.console.info(
-    'Extension name: ' + pj.name + ' with version: ' + pj.version
+    'Extension name ' + pj.name + ' with version ' + pj.version
   )
 
   const result: InitializeResult = {
