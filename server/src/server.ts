b2a5cdd7552f6012c30406eff499f9903b14a050
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-05-26 23:01:47 +0200

    add console info about binary hash (#176)

--- server/src/server.ts
+++ server/src/server.ts
@@ -57,6 +57,11 @@ connection.onInitialize(async (params: InitializeParams) => {
     }
   }
 
+  connection.console.info(
+    'Installed version of Prisma binary `prisma-fmt`: ' +
+    (await util.getVersion()),
+  )
+
   const pj = require('../../package.json')
   connection.console.info(
     'Extension name ' + pj.name + ' with version ' + pj.version
