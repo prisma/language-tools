445d6afbcb2536c33574a3eb2a6cf7b6f37a0bb1
Carmen Berndt <carmen@berndt-home.de>
2020-05-26 10:51:32 +0200

    add instructions to error output on formatting error

--- server/src/format.ts
+++ server/src/format.ts
@@ -9,15 +9,28 @@ export default async function format(
   try {
     return await exec(execPath, ['format', '-s', identWidth.toString()], text)
   } catch (errors) {
-    const errorMessage =
-      "prisma-fmt error'd during formatting. This was likely due to a syntax error. Please see Prisma Language Server output. To see the output, go to View > Output from the toolbar, then select 'Prisma Language Server' in the Output panel."
-    if (onError) {
-      onError(errorMessage)
+    if (
+      (typeof errors === 'string' || errors instanceof String) &&
+      errors.includes('ValidationError')
+    ) {
+      const errorMessage =
+        "\nprisma-fmt error'd during formatting. This was due to a syntax error."
+      console.warn(errorMessage)
+      if (onError) {
+        onError(errorMessage)
+      }
+    } else {
+      if (onError) {
+        onError(
+          "prisma-fmt error'd during formatting. Please see Prisma Language Server output. To see the output, go to View > Output from the toolbar, then select 'Prisma Language Server' in the Output panel.",
+        )
+      }
+      console.warn(
+        "\nprisma-fmt error'd during formatting. Please report this issue on [Prisma VSCode](https://github.com/prisma/vscode/issues). \nLinter output:\n",
+      )
+      console.warn(errors)
     }
-    console.warn(
-      "\nprisma-fmt error'd during formatting. This was likely due to a syntax error.\nLinter output:\n",
-    )
-    console.warn(errors)
+
     return text
   }
 }
