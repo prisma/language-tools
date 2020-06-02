5d1fa2cb23b412763ff2871ddf21e21aec4c8a05
Carmen Berndt <carmen@berndt-home.de>
2020-06-02 16:17:41 +0200

    use  instead of legacy

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -33,7 +33,7 @@ if [ "$CHANNEL" = "dev" ]; then
     jq ".version = \"$NEXT_EXTENSION_VERSION\" | \
         .name = \"prisma-insider\" | \
         .displayName = \"Prisma - Insider\" | \
-        .description = \"This is the Insider Build of the Prisma VSCode extension (only use it if you are also using the `dev` version of the CLI.\" | \
+        .description = \"This is the Insider Build of the Prisma VSCode extension (only use it if you are also using the $(dev) version of the CLI.\" | \
         .preview = true" \
         ./package.json > ./package.json.bk
 else
