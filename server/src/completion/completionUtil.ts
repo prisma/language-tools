e1997aff6fa6e415eaeec941cab0d84f096c73d6
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-06-04 13:22:57 +0200

    improvement(auto-completion): fix documentation and format of suggestions (#209)
    * adjust doc comments and format
    
    * Update server/src/completionUtil.ts
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>
    
    * add valid value for datasource provider in documentation
    
    * first try at moving out completions in json
    
    * move out all suggestions to json file
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

---
+++ server/src/completion/completionUtil.ts
@@ -0,0 +1,95 @@
+import {
+  CompletionItem,
+  CompletionItemKind,
+  MarkupKind,
+} from 'vscode-languageserver'
+import * as completions from './completions.json'
+
+/**
+ * Converts a json object containing labels and documentations to CompletionItems.
+ */
+function convertToCompletionItems(
+  completionItems: {
+    label: string
+    documentation: string
+  }[],
+  itemKind: CompletionItemKind,
+): CompletionItem[] {
+  const result: CompletionItem[] = []
+  for (const item of completionItems) {
+    result.push({
+      label: item.label,
+      kind: itemKind,
+      documentation: { kind: MarkupKind.Markdown, value: item.documentation },
+    })
+  }
+  return result
+}
+
+/**
+ * Converts a json object containing attributes including function signatures to CompletionItems.
+ */
+function convertAttributesToCompletionItems(
+  completionItems: {
+    label: string
+    documentation: string
+    fullSignature: string
+    params: { label: string; documentation: string }[]
+  }[],
+  itemKind: CompletionItemKind,
+): CompletionItem[] {
+  const result: CompletionItem[] = []
+  for (const item of completionItems) {
+    const docComment = [
+      '```prisma',
+      item.fullSignature,
+      '```',
+      '___',
+      item.documentation,
+    ]
+    for (const param of item.params) {
+      docComment.push('', '_@param_ ' + param.label + ' ' + param.documentation)
+    }
+    result.push({
+      label: item.label,
+      kind: itemKind,
+      insertText: item.label.replace('[]', '[$0]'),
+      insertTextFormat: 2,
+      documentation: {
+        kind: MarkupKind.Markdown,
+        value: docComment.join('\n'),
+      },
+    })
+  }
+  return result
+}
+
+export const corePrimitiveTypes: CompletionItem[] = convertToCompletionItems(
+  completions.primitiveTypes,
+  CompletionItemKind.TypeParameter,
+)
+
+export const allowedBlockTypes: CompletionItem[] = convertToCompletionItems(
+  completions.blockTypes,
+  CompletionItemKind.Class,
+)
+
+export const supportedDataSourceFields: CompletionItem[] = convertToCompletionItems(
+  completions.dataSourceFields,
+  CompletionItemKind.Field,
+)
+
+export const supportedGeneratorFields: CompletionItem[] = convertToCompletionItems(
+  completions.dataSourceFields,
+  CompletionItemKind.Field,
+)
+
+export const blockAttributes: CompletionItem[] = convertAttributesToCompletionItems(
+  completions.blockAttributes,
+  CompletionItemKind.Property,
+)
+
+export const fieldAttributes: CompletionItem[] = convertAttributesToCompletionItems(
+  completions.fieldAttributes,
+  CompletionItemKind.Property,
+)
