3591e95758d5bbeefa84769f22effc38e9db1a89
Carmen Berndt <carmen@berndt-home.de>
2020-06-09 18:04:28 +0200

    fix start of lsp from vscode

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -4,26 +4,27 @@ import {
   ServerOptions,
   TransportKind,
 } from 'vscode-languageclient'
-import {
-  ExtensionContext,
-  commands,
-  window,
-} from 'vscode'
+import { ExtensionContext, commands, window } from 'vscode'
 import path from 'path'
 
-function tryRequire(path: any) {
-  try {
-    return require(path)
-  } catch (err) {
-    console.error(err)
-    return
-  }
+function createLanguageServer(
+  serverOptions: ServerOptions,
+  clientOptions: LanguageClientOptions,
+) {
+  return new LanguageClient(
+    'prisma',
+    'Prisma Language Server',
+    serverOptions,
+    clientOptions,
+  )
 }
 
 export function activate(context: ExtensionContext) {
-  const serverModule = require.resolve('@prisma/language-server/bin/server.js')
+  //const serverModule = require.resolve('@prisma/language-server/bin/server.js')
   // for debugging the server
-  //const serverModule = require(path.join(__dirname, '../../../language-server/bin/server.js'))
+  const serverModule = context.asAbsolutePath(
+    path.join('../../packages/language-server/dist/src/cli.js'),
+  )
 
   // The debug options for the server
   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
@@ -57,18 +58,11 @@ export function activate(context: ExtensionContext) {
 
   context.subscriptions.push(
     commands.registerCommand('prisma.restartLanguageServer', async () => {
-        await client.stop();
-        client = createLanguageServer(serverOptions, clientOptions);
-        context.subscriptions.push(client.start());
-        await client.onReady();
-        window.showInformationMessage('Prisma language server restarted.');
+      await client.stop()
+      client = createLanguageServer(serverOptions, clientOptions)
+      context.subscriptions.push(client.start())
+      await client.onReady()
+      window.showInformationMessage('Prisma language server restarted.')
     }),
-);
-}
-
-function createLanguageServer(serverOptions: ServerOptions, clientOptions: LanguageClientOptions) {
-  return new LanguageClient('prisma',
-    'Prisma Language Server',
-    serverOptions,
-    clientOptions)
+  )
 }
