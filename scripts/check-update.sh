cdf909da1a795a9a0d0d5ddc1ef4cbdbb3d445fc
Carmen Berndt <carmen@berndt-home.de>
2020-06-17 20:04:23 +0200

    bring back printing of next extension version for log purposes

--- scripts/check-update.sh
+++ scripts/check-update.sh
@@ -37,6 +37,8 @@ else
 fi
 
 if [ "$CURRENT_VERSION" != "$NPM_VERSION" ]; then
+    NEXT_EXTENSION_VERSION=$(node scripts/extension-version.js "$NPM_VERSION" "$EXTENSION_VERSION")
+    echo "NEXT_EXTENSION_VERSION: $NEXT_EXTENSION_VERSION"
     echo "::set-output name=new_updates::true"
 else
     echo "::set-output name=new_updates::false"
