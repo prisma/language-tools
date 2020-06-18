af75a13aa698905d53457a30d4c6ee9391468c5f
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-06-18 15:32:56 +0200

    feat(CI): add different readme to unstable extension (#234)
    * add different readme to unstable extension
    
    * removed unused files
    
    * fix fucntion call
    
    * fix path to logo for stable readme
    
    * fix path to contributing file
    
    * use separate readmes for marketplace and github
    
    * add security info for marketplace readmes
    
    * add link to marketplace to github readme
    
    * Update scripts/README_STABLE_BUILD.md
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>
    
    * remove note about new Prisma syntax
    
    * remove note about new prisma 2 datamodel syntax from readme
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>

---
+++ scripts/change-readme.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const githubAction = require('@actions/github')
+
+
+function changeReadme({
+  releaseChannel
+}) {
+  if (releaseChannel === 'dev') {
+    let content = fs.readFileSync(
+      join(__dirname, "./scripts/README_INSIDER_BUILD.md"),
+      {
+        encoding: "utf-8",
+      }
+    );
+    content = content.replace(/\$commit-sha\$/g, githubAction.context.sha)
+    fs.writeFileSync( "./README.md", content);
+  } else {
+     content = fs.readFileSync(
+      join(__dirname, "./scripts/README_STABLE_BUILD.md"),
+      {
+        encoding: "utf-8",
+      }
+    )
+    fs.writeFileSync("./README.md", content);
+  }
+}
+
+
+module.exports = { changeReadme }
+
+if (require.main === module) {
+  const args = process.argv.slice(1)
+  changeReadme({
+    releaseChannel: args[0]
+  })
+}
