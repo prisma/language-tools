9399679c00fea14b319da7b919c9426af7f96bac
Carmen Berndt <carmen@berndt-home.de>
2020-06-19 15:44:22 +0200

    create separate function to get lsp connection

--- packages/language-server/src/server.ts
+++ packages/language-server/src/server.ts
@@ -1,11 +1,11 @@
 import {
-    IConnection,
-    TextDocuments,
-    DiagnosticSeverity,
-    Diagnostic,
-    createConnection,
-    IPCMessageReader,
-    IPCMessageWriter
+  IConnection,
+  TextDocuments,
+  DiagnosticSeverity,
+  Diagnostic,
+  createConnection,
+  IPCMessageReader,
+  IPCMessageWriter,
 } from 'vscode-languageserver'
 import { TextDocument } from 'vscode-languageserver-textdocument'
 import * as MessageHandler from './MessageHandler'
@@ -16,11 +16,24 @@ import fs from 'fs'
 import install from './install'
 
 export interface LSOptions {
-    /**
-     * If you have a connection already that the ls should use, pass it in.
-     * Else the connection will be created from `process`.
-     */
-    connection?: IConnection;
+  /**
+   * If you have a connection already that the ls should use, pass it in.
+   * Else the connection will be created from `process`.
+   */
+  connection?: IConnection
+}
+
+function getConnection(options?: LSOptions): IConnection {
+  let connection = options?.connection
+  if (!connection) {
+    connection = process.argv.includes('--stdio')
+      ? createConnection(process.stdin, process.stdout)
+      : createConnection(
+          new IPCMessageReader(process),
+          new IPCMessageWriter(process),
+        )
+  }
+  return connection
 }
 
 /**
@@ -29,128 +42,123 @@ export interface LSOptions {
  * @param options Options to customize behavior
  */
 export function startServer(options?: LSOptions) {
-    let connection = options?.connection;
-    if (!connection) {
-        connection = process.argv.includes('--stdio')
-            ? createConnection(process.stdin, process.stdout)
-            : createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
+  const connection: IConnection = getConnection(options)
+  const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
+
+  connection.onInitialize(async () => {
+    const binPathPrismaFmt = await util.getBinPath()
+    if (!fs.existsSync(binPathPrismaFmt)) {
+      try {
+        await install(binPathPrismaFmt)
+        connection.console.info(
+          'Prisma plugin prisma-fmt installation succeeded.',
+        )
+      } catch (err) {
+        connection.console.error('Cannot install prisma-fmt: ' + err)
+      }
     }
 
-    const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
+    connection.console.info(
+      'Installed version of Prisma binary `prisma-fmt`: ' +
+        (await util.getVersion()),
+    )
 
-    connection.onInitialize(async () => {
-        const binPathPrismaFmt = await util.getBinPath()
-        if (!fs.existsSync(binPathPrismaFmt)) {
-            try {
-                await install(binPathPrismaFmt)
-                connection!.console.info(
-                    'Prisma plugin prisma-fmt installation succeeded.',
-                )
-            } catch (err) {
-                connection!.console.error('Cannot install prisma-fmt: ' + err)
-            }
-        }
+    const pj = util.tryRequire('../../package.json')
+    connection.console.info(
+      'Extension name ' + pj.name + ' with version ' + pj.version,
+    )
+    const prismaCLIVersion = await util.getCLIVersion()
+    connection.console.info('Prisma CLI version: ' + prismaCLIVersion)
 
-        connection!.console.info(
-            'Installed version of Prisma binary `prisma-fmt`: ' +
-            (await util.getVersion()),
-        )
+    return {
+      capabilities: {
+        definitionProvider: true,
+        documentFormattingProvider: true,
+        completionProvider: {
+          resolveProvider: true,
+          triggerCharacters: ['@', '"'],
+        },
+        hoverProvider: true,
+      },
+    }
+  })
 
-        const pj = util.tryRequire('../../package.json')
-        connection!.console.info(
-            'Extension name ' + pj.name + ' with version ' + pj.version,
-        )
-        const prismaCLIVersion = await util.getCLIVersion()
-        connection!.console.info('Prisma CLI version: ' + prismaCLIVersion)
+  async function validateTextDocument(
+    textDocument: TextDocument,
+  ): Promise<void> {
+    const text = textDocument.getText(fullDocumentRange(textDocument))
+    const binPath = await util.getBinPath()
 
-        return {
-            capabilities: {
-                definitionProvider: true,
-                documentFormattingProvider: true,
-                completionProvider: {
-                    resolveProvider: true,
-                    triggerCharacters: ['@', '"'],
-                },
-                hoverProvider: true,
-            },
-        };
-    });
-
-
-    documents.onDidChangeContent((change: { document: any }) => {
-        validateTextDocument(change.document)
+    const res = await lint(binPath, text, (errorMessage: string) => {
+      connection.window.showErrorMessage(errorMessage)
     })
 
-    documents.onDidOpen((open: { document: any }) => {
-        validateTextDocument(open.document)
-    })
-
-    connection.onDefinition((params: any) =>
-        MessageHandler.handleDefinitionRequest(documents, params),
-    )
-
-    connection.onCompletion((params: any) =>
-        MessageHandler.handleCompletionRequest(params, documents),
-    )
-
-    connection.onCompletionResolve((params: any) =>
-        MessageHandler.handleCompletionResolveRequest(params),
-    )
-
-    connection.onHover((params: any) =>
-        MessageHandler.handleHoverRequest(documents, params),
-    )
-
-    connection.onDocumentFormatting((params: any) =>
-        MessageHandler.handleDocumentFormatting(
-            params,
-            documents,
-            (errorMessage: string) => {
-                connection!.window.showErrorMessage(errorMessage)
-            },
-        ),
-    )
-
-    async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-        const text = textDocument.getText(fullDocumentRange(textDocument))
-        const binPath = await util.getBinPath()
-
-        const res = await lint(binPath, text, (errorMessage: string) => {
-            connection!.window.showErrorMessage(errorMessage)
-        })
-
-        const diagnostics: Diagnostic[] = []
-        if (
-            res.some(
-                (err) =>
-                    err.text === "Field declarations don't require a `:`." ||
-                    err.text ===
-                    'Model declarations have to be indicated with the `model` keyword.',
-            )
-        ) {
-            connection!.window.showErrorMessage(
-                "You are currently viewing a Prisma 1 datamodel which is based on the GraphQL syntax. The current Prisma VSCode extension doesn't support this syntax. To get proper syntax highlighting for this file, please change the file extension to `.graphql` and download the [GraphQL VSCode extension](https://marketplace.visualstudio.com/items?itemName=Prisma.vscode-graphql). Learn more [here](https://pris.ly/prisma1-vscode).",
-            )
-        }
-
-        for (const error of res) {
-            const diagnostic: Diagnostic = {
-                severity: DiagnosticSeverity.Error,
-                range: {
-                    start: textDocument.positionAt(error.start),
-                    end: textDocument.positionAt(error.end),
-                },
-                message: error.text,
-                source: '',
-            }
-            diagnostics.push(diagnostic)
-        }
-        connection!.sendDiagnostics({ uri: textDocument.uri, diagnostics })
+    const diagnostics: Diagnostic[] = []
+    if (
+      res.some(
+        (err) =>
+          err.text === "Field declarations don't require a `:`." ||
+          err.text ===
+            'Model declarations have to be indicated with the `model` keyword.',
+      )
+    ) {
+      connection.window.showErrorMessage(
+        "You are currently viewing a Prisma 1 datamodel which is based on the GraphQL syntax. The current Prisma VSCode extension doesn't support this syntax. To get proper syntax highlighting for this file, please change the file extension to `.graphql` and download the [GraphQL VSCode extension](https://marketplace.visualstudio.com/items?itemName=Prisma.vscode-graphql). Learn more [here](https://pris.ly/prisma1-vscode).",
+      )
     }
 
-    // Make the text document manager listen on the connection
-    // for open, change and close text document events
-    documents.listen(connection)
+    for (const error of res) {
+      const diagnostic: Diagnostic = {
+        severity: DiagnosticSeverity.Error,
+        range: {
+          start: textDocument.positionAt(error.start),
+          end: textDocument.positionAt(error.end),
+        },
+        message: error.text,
+        source: '',
+      }
+      diagnostics.push(diagnostic)
+    }
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics })
+  }
 
-    connection.listen();
+  documents.onDidChangeContent((change: { document: any }) => {
+    validateTextDocument(change.document)
+  })
+
+  documents.onDidOpen((open: { document: any }) => {
+    validateTextDocument(open.document)
+  })
+
+  connection.onDefinition((params: any) =>
+    MessageHandler.handleDefinitionRequest(documents, params),
+  )
+
+  connection.onCompletion((params: any) =>
+    MessageHandler.handleCompletionRequest(params, documents),
+  )
+
+  connection.onCompletionResolve((params: any) =>
+    MessageHandler.handleCompletionResolveRequest(params),
+  )
+
+  connection.onHover((params: any) =>
+    MessageHandler.handleHoverRequest(documents, params),
+  )
+
+  connection.onDocumentFormatting((params: any) =>
+    MessageHandler.handleDocumentFormatting(
+      params,
+      documents,
+      (errorMessage: string) => {
+        connection.window.showErrorMessage(errorMessage)
+      },
+    ),
+  )
+
+  // Make the text document manager listen on the connection
+  // for open, change and close text document events
+  documents.listen(connection)
+
+  connection.listen()
 }
