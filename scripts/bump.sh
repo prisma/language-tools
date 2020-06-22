0433b1646504a4d12ca69756341660650d8e5a34
Carmen Berndt <carmen@berndt-home.de>
2020-06-22 13:58:37 +0200

    fix indent

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -52,7 +52,7 @@ else
     node scripts/change-readme.js "$RELEASE_CHANNEL"
 fi
 
-    echo "::set-output name=version::$NEXT_EXTENSION_VERSION"
+echo "::set-output name=version::$NEXT_EXTENSION_VERSION"
 
 jq ".version = \"$NEXT_EXTENSION_VERSION\" | \
     .prisma.version = \"$SHA\" | \
