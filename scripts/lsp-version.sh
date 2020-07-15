f4cc5760c2a19fd5ef1b035e5460ce8af4c2995b
Carmen Berndt <carmen@berndt-home.de>
2020-07-15 16:13:12 +0200

    ci: check existing lsp version before publish

---
+++ scripts/lsp-version.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu
+
+VERSION=$1
+
+REGISTRY_INFO=$(npm view @prisma/language-server@$VERSION)
+
+echo "$REGISTRY_INFO"
+
+if [ "$REGISTRY_INFO" = "" ]; then
+    echo "LSP not published yet."
+    echo "::set-output name=lsp-not-published::true"
+else
+    echo "LSP with version $VERSION is published already."
+fi
