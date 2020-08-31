88436a0000b7636fa618c335715a32fd4493a645
Carmen Berndt <carmen@berndt-home.de>
2020-08-31 10:04:32 +0200

    minor fixes

--- scripts/versions/bump_prisma_version.sh
+++ scripts/versions/bump_prisma_version.sh
@@ -8,7 +8,7 @@ echo "NPM_CHANNEL: $NPM_CHANNEL"
 NEW_NPM_VERSION=$2
 echo "NEW_NPM_VERSION: $NEW_NPM_VERSION"
 
-echo "UPDATING to Prisma CLI version $NPM_VERSION"
+echo "UPDATING to Prisma CLI version $NEW_NPM_VERSION"
 
 
 if [ "$NPM_CHANNEL" = "dev" ]; then
