219518455f4746c5e6d810d481627d8e8d1ae898
Carmen Berndt <carmen@berndt-home.de>
2020-09-02 10:50:51 +0200

    have lsp and extension on same version

---
+++ scripts/bump_lsp_version.js
@@ -0,0 +1,12 @@
+import { bumpExtensionVersionInScriptFiles } from './versions/bump_extension_version'
+
+
+if (require.main === module) {
+  const args = process.argv.slice(2)
+  bumpExtensionVersionInScriptFiles({
+    nextVersion: args[0],
+    branch_channel: args[1],
+    extensionBump: false
+  })
+  return version
+}
