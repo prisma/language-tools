c409b9b9267bb78680fd9a1c01153b3bf91e7bb5
Carmen Berndt <carmen@berndt-home.de>
2020-09-02 14:26:46 +0200

    fix more typos

--- scripts/get_tag_name.sh
+++ scripts/get_tag_name.sh
@@ -9,9 +9,9 @@ VSCODE_VERSION=$2
 echo "VSCODE_VERSION: $VSCODE_VERSION"
 
 if [ "$RELEASE_CHANNEL" = "latest" ]; then 
-    echo "::set-output name=tag-name::$VSCODE_VERSION"
-    echo "::set-output name=asset-name::prisma"
+    echo "::set-output name=tag_name::$VSCODE_VERSION"
+    echo "::set-output name=asset_name::prisma"
 else
-    echo "::set-output name=tag-name::insider/$VSCODE_VERSION"
-    echo "::set-output name=asset-name::prisma-insider"
+    echo "::set-output name=tag_name::insider/$VSCODE_VERSION"
+    echo "::set-output name=asset_name::prisma-insider"
 fi
