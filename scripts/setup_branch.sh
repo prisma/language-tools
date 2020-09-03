c3efe04c1ddd9f099515325a9eeda3c35ce43eb7
Carmen Berndt <carmen@berndt-home.de>
2020-09-03 10:59:41 +0200

    fix typo

--- scripts/setup_branch.sh
+++ scripts/setup_branch.sh
@@ -28,7 +28,7 @@ if [ "${EXISTS_ALREADY}" = "" ]; then
         git config --global user.email "prismabots@gmail.com"
         git config --global user.name "Prismo"
 
-        if [ "$NPM_CHANNEL" = "latest"]; then
+        if [ "$NPM_CHANNEL" = "latest" ]; then
             git checkout -b "$BRANCH"
         else
             # Patch branch
