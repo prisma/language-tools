bc54e42527f1ae95624154f34f41a6d22c5b48ce
Carmen Berndt <carmen@berndt-home.de>
2020-09-08 15:51:18 +0200

    remove extraction

--- scripts/bump_prisma_version_in_scripts.js
+++
@@ -1,8 +0,0 @@
-import { writeToVersionFile } from './util'
-
-
-if (require.main === module) {
-    const args = process.argv.slice(2)
-    writeToVersionFile(`prisma_${args[0]}`, args[1])
-    console.log(`Updating to Prisma CLI version ${args[1]} for channel ${args[0]}.`)
-}
