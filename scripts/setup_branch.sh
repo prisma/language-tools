9f3fbca2d4b7b1ea91c6bf8f89b0964b27e52c99
Carmen Berndt <carmen@berndt-home.de>
2020-09-10 13:19:46 +0200

    fix shell script

--- scripts/setup_branch.sh
+++ scripts/setup_branch.sh
@@ -16,7 +16,7 @@ NPM_CHANNEL=$1
 if [ "$NPM_CHANNEL" = "dev" ]; then
     echo "Not switching branch because we are on NPM_CHANNEL dev."
     echo "::set-output name=branch::"
-elif [ "$NPM_CHANNEL" = "promote_patch-dev"]; then
+elif [ "$NPM_CHANNEL" = "promote_patch-dev" ]; then
     PATCH_BRANCH=$(node scripts/setup_branch.js "patch-dev")
     git checkout stable
     git reset --hard "$PATCH_BRANCH" # Reset stable to patch-dev branch
