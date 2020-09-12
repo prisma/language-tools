32ce67d334178567fb40d4570d3067777c116e5d
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-09-12 19:43:20 +0200

    fix: GH action default parameter (#460)
    * fix: set parameter to default master
    
    * fix
    
    * Update .github/workflows/1_2_c_promote_patch_to_stable.yml

--- scripts/commit_and_push.sh
+++ scripts/commit_and_push.sh
@@ -5,20 +5,25 @@ set -eu
 
 # For local development, in production, the environment will be set though GH actions and GH secrets
 if [ -f ".envrc" ]; then
-    echo "Loading .envrc"
-    # shellcheck disable=SC1091
-    . .envrc
+  echo "Loading .envrc"
+  # shellcheck disable=SC1091
+  . .envrc
 else
-    echo "No .envrc"
+  echo "No .envrc"
 fi
 
 COMMIT_MESSAGE=$1
 echo "COMMIT_MESSAGE: $COMMIT_MESSAGE"
 
-BRANCH=$2
+BRANCH=${2-master}
 echo "BRANCH: $BRANCH"
 
 git add -A .
 git commit -am "$COMMIT_MESSAGE"
 git pull --rebase
-git push $BRANCH
+
+if [ $BRANCH = "master" ]; then
+  git push
+else
+  git push $BRANCH
+fi
