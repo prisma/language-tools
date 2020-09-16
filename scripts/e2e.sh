a9fff47a581841175669c7b132aaacd27e9a145f
Carmen Berndt <carmen@berndt-home.de>
2020-09-16 13:27:31 +0200

    fix path to e2e tests

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -10,8 +10,8 @@ export CODE_TESTS_WORKSPACE
 
 if [ "$USE_LOCAL" = "useLocalLSP" ]; then 
     echo "Using local LSP."
-    node "$(pwd)/packages/vscode/dist/src/test/runTest" "true"
+    node "$(pwd)/packages/vscode/dist/test/runTest" "true"
 else 
     echo "Using published LSP."
-    node "$(pwd)/packages/vscode/dist/src/test/runTest" "false"
+    node "$(pwd)/packages/vscode/dist/test/runTest" "false"
 fi
