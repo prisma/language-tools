d933faed38cc677d301968129a21ab7af7f92cee
Carmen Berndt <carmen@berndt-home.de>
2020-09-16 13:31:40 +0200

    fix path

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -2,7 +2,7 @@
 
 USE_LOCAL=${1-false}
 
-CODE_TESTS_PATH="$(pwd)/packages/vscode/dist/src/test"
+CODE_TESTS_PATH="$(pwd)/packages/vscode/dist/test"
 export CODE_TESTS_PATH
 
 CODE_TESTS_WORKSPACE="$(pwd)/packages/vscode/testFixture"
