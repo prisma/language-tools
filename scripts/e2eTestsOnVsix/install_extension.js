88d893c6b31647a3c980d8241d3ed4ba456661d8
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-09-28 09:21:46 +0200

    chore: move e2e tests to language tools repo (#487)
    * chore: move e2e tests to language tools repo
    
    * add note in Contributing about e2e tests

---
+++ scripts/e2eTestsOnVsix/install_extension.js
@@ -0,0 +1,51 @@
+const vscodeTest = require('vscode-test')
+const chileProcess = require('child_process')
+
+
+async function installExtension({
+  extensionType,
+  extensionVersion,
+}) {
+  try {
+    let vsceArgument = ''
+    if (type === 'insider') {
+      vsceArgument = '-insider'
+    }
+    const extensionName = `Prisma.prisma${vsceArgument}`
+
+    // Install VSCode
+    const vscodeExecutablePath = await vscodeTest.downloadAndUnzipVSCode("stable")
+
+    // Install VSCode extension
+    const cliPath = vscodeTest.resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath)
+    const result = chileProcess.spawnSync(cliPath, ['--install-extension', extensionName + '@' + extensionVersion], {
+      encoding: 'utf-8',
+      stdio: 'pipe'
+    })
+    console.log(result)
+    if (result.stderr.includes('Failed')) {
+      console.log("It's not ready to be installed yet.")
+      return "FAIL"
+    } else {
+      console.log("::set-output name=installed-extension::true")
+      return "SUCCESS"
+    }
+  } catch (err) {
+    console.error('Failed to install the extension.')
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+
+module.exports = { installExtension }
+
+if (require.main === module) {
+  const args = process.argv.slice(2)
+  installExtension({
+    extensionType: args[0],
+    extensionVersion: args[1],
+  }).then(res => {
+    console.log(res)
+  })
+}
