76d51c868d87daa6742b016dce09e66be2c957c1
Carmen Berndt <carmen@berndt-home.de>
2020-09-28 10:29:18 +0200

    fix: e2e tests after publish

--- scripts/e2eTestsOnVsix/check_for_new_release.js
+++ scripts/e2eTestsOnVsix/check_for_new_release.js
@@ -1,27 +1,20 @@
 const { readVersionFile } = require('./../util')
-const execa = require('execa')
 
-function checkForNewRelease({ type }) {
-  (async () => {
-    const lastTestedVersion = readVersionFile({ fileName: `tested_extension_${type}` })
-    console.log(`Last tested ${type} version: ${lastTestedVersion}`)
-    let vsceArgument = ''
-    if (type === 'insider') {
-      vsceArgument = '-insider'
-    }
-    const { stdout } = await execa('vsce', ['show', `Prisma.prisma${vsceArgument}`, '--json']);
-    const publishedVersion = JSON.parse(stdout).versions[0].version
-    console.log(`Published ${type} version: ${publishedVersion}`)
-    if (lastTestedVersion != publishedVersion) {
-      console.log(`New published version for ${type} available.`)
-      console.log(`::set-output name=new_${type}_version::${publishedVersion}`)
-    }
-  })();
+function checkForNewRelease({ type, version }) {
+
+  const lastTestedVersion = readVersionFile({ fileName: `tested_extension_${type}` })
+  console.log(`Last tested ${type} version: ${lastTestedVersion}`)
+  console.log(`Published ${type} version: ${version}`)
+  if (lastTestedVersion != version) {
+    console.log(`New published version for ${type} available.`)
+    console.log(`::set-output name=new_${type}_version::${version}`)
+  }
 }
 
 module.exports = { checkForNewRelease }
 
 if (require.main === module) {
-  checkForNewRelease({ type: 'insider' })
-  checkForNewRelease({ type: 'stable' })
+  const args = process.argv.slice(2)
+  checkForNewRelease({ type: 'insider', version: args[0] })
+  checkForNewRelease({ type: 'stable', version: args[1] })
 }
