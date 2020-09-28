e49c3ba6e5159cdbb434618defa68dc03da8f1a5
carmenberndt <30771368+carmenberndt@users.noreply.github.com>
2020-09-28 10:54:39 +0200

    fix: add log output in case of wrong argument type (#495)

--- scripts/e2eTestsOnVsix/check_for_new_release.js
+++ scripts/e2eTestsOnVsix/check_for_new_release.js
@@ -1,7 +1,9 @@
 const { readVersionFile } = require('./../util')
 
 function checkForNewRelease({ type, version }) {
-
+  if (version === undefined) {
+    throw new Error(`Version for ${type} is undefined.`)
+  }
   const lastTestedVersion = readVersionFile({ fileName: `tested_extension_${type}` })
   console.log(`Last tested ${type} version: ${lastTestedVersion}`)
   console.log(`Published ${type} version: ${version}`)
@@ -15,6 +17,11 @@ module.exports = { checkForNewRelease }
 
 if (require.main === module) {
   const args = process.argv.slice(2)
+  if (args.length != 2) {
+    throw new Error(`Expected two arguments, received ${args.length}`)
+  }
+  console.log(`Published Insider version: ${args[0]}.`)
+  console.log(`Published stable version: ${args[1]}.`)
   checkForNewRelease({ type: 'insider', version: args[0] })
   checkForNewRelease({ type: 'stable', version: args[1] })
 }
