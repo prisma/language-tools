22110c84f66b753e8c951bc48d01e8332de8b10d
carmenberndt <carmen@berndt-home.de>
2020-10-01 16:29:20 +0200

    feat: spelling suggestion for wrong block names (#518)
    * feat: spelling suggestion for wrong block names
    
    * apply Divvies feedback

--- packages/language-server/src/codeActionProvider.ts
+++ packages/language-server/src/codeActionProvider.ts
@@ -35,16 +35,16 @@ function getInsertRange(document: TextDocument): Range {
  *        (0.4 allows 1 substitution/transposition for every 5 characters,
  *         and 1 insertion/deletion at 3 characters)
  */
-function getSpellingSuggestionForModelAndEnumName(
+function getSpellingSuggestions(
   name: string,
-  relations: string[],
+  possibleSuggestions: string[],
 ): string | undefined {
   const maximumLengthDifference = Math.min(2, Math.floor(name.length * 0.34))
   let bestDistance = Math.floor(name.length * 0.4) + 1 // If the best result isn't better than this, don't bother.
   let bestCandidate: string | undefined
   let justCheckExactMatches = false
   const nameLowerCase = name.toLowerCase()
-  for (const candidate of relations) {
+  for (const candidate of possibleSuggestions) {
     if (
       !(
         Math.abs(candidate.length - nameLowerCase.length) <=
@@ -129,7 +129,7 @@ export function quickFix(
         hasTypeModifierOptional,
         diagText,
       )
-      const spellingSuggestion = getSpellingSuggestionForModelAndEnumName(
+      const spellingSuggestion = getSpellingSuggestions(
         diagText,
         getAllRelationNames(lines),
       )
@@ -257,6 +257,34 @@ export function quickFix(
           },
         },
       })
+    }
+    if (diag.severity === DiagnosticSeverity.Error &&
+      diag.message.includes('It does not start with any known Prisma schema keyword.')) {
+      let diagText = textDocument.getText(diag.range).split(/\s/)
+      if (diagText.length !== 0) {
+        const spellingSuggestion = getSpellingSuggestions(
+          diagText[0],
+          ["model", "enum", "datasource", "generator"],
+        )
+        if (spellingSuggestion) {
+          codeActions.push({
+            title: `Change spelling to '${spellingSuggestion}'`,
+            kind: CodeActionKind.QuickFix,
+            diagnostics: [diag],
+            edit: {
+              changes: {
+                [params.textDocument.uri]: [
+                  {
+                    range: { start: diag.range.start, end: { line: diag.range.start.line, character: diagText[0].length } },  // the red squiggly lines start at the beginning of the blog and end at the end of the line, include e.g. 'mode nameOfBlock {' but 
+                    // we only want to replace e.g. 'mode' with 'model', not delete the whole line
+                    newText: spellingSuggestion,
+                  },
+                ],
+              },
+            },
+          })
+        }
+      }
     }
   }
 
