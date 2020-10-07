1390286252b6f9580a52a9103100cb0384dd9517
William Luke <william@atto-byte.com>
2020-10-07 16:53:18 +0300

    Revert "feat(vscode): pluginify services" (#535)

--- packages/vscode/src/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import languageServer from './prisma-language-server'
-import { PrismaVSCodePlugin } from './types'
-
-const plugins: PrismaVSCodePlugin[] = [languageServer]
-
-export default plugins
