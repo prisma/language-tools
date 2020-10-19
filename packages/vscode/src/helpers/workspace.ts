ab7dc6d7ac07c4cdc547991bcb754b7b00cc77df
William Luke <william@ordino.ai>
2020-10-19 15:33:32 +0300

    fix: module not found

--- packages/vscode/src/helpers/workspace.ts
+++ packages/vscode/src/helpers/workspace.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import path from 'path'
 import { workspace } from 'vscode'
 type PackageScopes = 'devDependencies' | 'dependencies' | 'peerDependencies'
@@ -6,8 +7,9 @@ export async function packageJsonIncludes(
   scopes: PackageScopes[],
 ): Promise<boolean> {
   const rootPath = workspace.rootPath
-  if (rootPath) {
-    const packageJson = await require(path.join(rootPath, 'package.json')) // eslint-disable-line
+  const pkgPath = rootPath && path.join(rootPath, 'package.json')
+  if (rootPath && pkgPath && fs.existsSync(pkgPath)) {
+    const packageJson = await require(pkgPath) // eslint-disable-line
     for (const scope of scopes) {
       // eslint-disable-next-line
       const dependencies = packageJson && packageJson[scope]
