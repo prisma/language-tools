df13bd4bae93e657a633b0561e9f9f72bbf23226
William Luke <william@ordino.ai>
2020-11-03 07:14:36 +0300

    feat(vscode): remove nextjs

--- packages/vscode/src/plugins/index.ts
+++ packages/vscode/src/plugins/index.ts
@@ -1,7 +1,6 @@
-import nextjs from './nextjs'
 import languageServer from './prisma-language-server'
 import { PrismaVSCodePlugin } from './types'
 
-const plugins: PrismaVSCodePlugin[] = [languageServer, nextjs]
+const plugins: PrismaVSCodePlugin[] = [languageServer]
 
 export default plugins
