a89b86d84161668d7c65f74f47353db574edc703
carmenberndt <carmen@berndt-home.de>
2021-03-16 11:44:25 +0100

    chore: CI test for version incompatibilities (#742)
    * update snapshots
    
    * test: unit test for version incompatibilities
    
    * cleanup

---
+++ scripts/__tests__/dependencies.test.js
@@ -0,0 +1,13 @@
+var semver = require('semver')
+
+describe('version incompatibilities', () => {
+
+    it('@types/vscode should be less than or equal to engine.vscode version', () => {
+        const packageJson = require("../../packages/vscode/package.json")
+        const vscodeTypes = packageJson.devDependencies['@types/vscode']
+        const vscodeEngine = packageJson.engines.vscode
+        expect(
+            semver.ltr(vscodeTypes, vscodeEngine) || semver.eq(vscodeTypes, vscodeEngine.substring(1))
+        ).toBeTruthy()
+    })
+})
