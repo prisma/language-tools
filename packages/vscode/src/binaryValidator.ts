527b4bc080b2bdeb184d0e23ed9b27fd365389b6
Harshit Pant <pantharshit00@gmail.com>
2021-03-25 17:21:42 +0530

    Update packages/vscode/src/binaryValidator.ts
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- packages/vscode/src/binaryValidator.ts
+++ packages/vscode/src/binaryValidator.ts
@@ -55,6 +55,6 @@ export const checkAndAskForBinaryExecution = async (
 
 export const printBinaryCheckWarning = async () => {
   await window.showWarningMessage(
-    "Execution of Prisma extension's binary defined in settings was not allowed. Please either change the binary path or reset the approval by opening the VS Code command palette and search for 'Reset Binary execution decisions', then press Enter.",
+    "Execution of Prisma extension's binary defined in settings was not allowed. Please either change the binary path or reset the approval by opening the VS Code command palette and search for 'Reset current Prisma format binary execution decision', then press Enter.",
   )
 }
