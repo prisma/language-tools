cbe80416072e074fcf577ae875fad1e5133f4795
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2021-11-23 12:35:52 +0100

    feat: Log when one of the functions exported from prisma-fmt-wasm is called (#956)

--- packages/language-server/src/prisma-fmt/referentialActions.ts
+++ packages/language-server/src/prisma-fmt/referentialActions.ts
@@ -4,6 +4,7 @@ export default function referentialActions(
   text: string,
   onError?: (errorMessage: string) => void,
 ): string[] {
+  console.log("running referential_actions() from prisma-fmt");
   const result = prismaFmt.referential_actions(text)
   return JSON.parse(result) as string[]
 }
