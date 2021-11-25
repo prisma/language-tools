2337a6f091430cb099e95abb2afc28a1c161f0e3
Tom Houlé <13155277+tomhoule@users.noreply.github.com>
2021-11-25 10:48:01 +0100

    fix: Re-calibrate error handling to referentialActions() and nativeTypes()… (#966)
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- packages/language-server/src/prisma-fmt/referentialActions.ts
+++ packages/language-server/src/prisma-fmt/referentialActions.ts
@@ -4,7 +4,15 @@ export default function referentialActions(
   text: string,
   onError?: (errorMessage: string) => void,
 ): string[] {
-  console.log("running referential_actions() from prisma-fmt");
-  const result = prismaFmt.referential_actions(text)
-  return JSON.parse(result) as string[]
+  try {
+    console.log('running referential_actions() from prisma-fmt')
+    const result = prismaFmt.referential_actions(text)
+    return JSON.parse(result) as string[]
+  } catch (err) {
+    if (onError) {
+      onError(`prisma-fmt error'd during getting available referential actions. ${err}`)
+    }
+
+    return []
+  }
 }
