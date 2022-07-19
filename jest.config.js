b42f600e25eb750a08ad94fe24b02af7144918b1
Joël Galeran <Jolg42@users.noreply.github.com>
2022-07-19 15:11:08 +0200

    add codecov reports

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverage: process.env.CI ? true : false,
+  coverageReporters: ['clover'],
+  coverageDirectory: 'scripts/__tests__/coverage',
+  collectCoverageFrom: ['scripts/**/*.js', '!**/__tests__/**/*'],
+  reporters: ['default', 'github-actions'],
 }
