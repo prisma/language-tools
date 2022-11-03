9ebec5dc354c27b13d3dd056b7030d02eeaca535
Sophie Atkins <atkins@prisma.io>
2022-11-03 12:21:35 -0400

    linting

--- packages/language-server/src/__test__/quickFix.test.ts
+++ packages/language-server/src/__test__/quickFix.test.ts
@@ -218,7 +218,7 @@ suite('Quick Fixes', () => {
 
       let character = 11
 
-      if (process.platform === "win32") {
+      if (process.platform === 'win32') {
         character = 12
       }
 
@@ -240,7 +240,8 @@ suite('Quick Fixes', () => {
                   },
                 },
                 severity: 1,
-                message: 'Error parsing attribute "@relation": The argument `references` must refer to a unique criteria in the related model. Consider adding an `@unique` attribute to the field `field` in the model `A`.',
+                message:
+                  'Error parsing attribute "@relation": The argument `references` must refer to a unique criteria in the related model. Consider adding an `@unique` attribute to the field `field` in the model `A`.',
               },
             ],
             edit: {
