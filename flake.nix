da69b77ff2a3fbce68082d476abc92a1a94e0776
Tom Houlé <tom@tomhoule.com>
2022-12-02 09:38:12 +0100

    nix: write prisma-language-server derivation
    That makes it very easy for nix users to reproducibly build and run the
    language server. See the new section in CONTRIBUTING.md for more info.
    
    Good next steps would be integrating test runs and improving the dev
    shell.

--- flake.nix
+++ flake.nix
@@ -7,16 +7,29 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+
+        # Avoid fetching large unused projects.
+        all-cabal-json.follows = "nixpkgs";
+        nix-pypi-fetcher.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-parts }:
+  outputs = { self, nixpkgs, flake-parts, dream2nix }:
     flake-parts.lib.mkFlake { inherit self; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, system, self', ... }:
+      imports = [ dream2nix.flakeModuleBeta ];
+      perSystem = { ... }:
         {
           imports = [
-            ./nix/format-project.nix
             ./nix/delete-node-modules.nix
+            ./nix/format-project.nix
+            ./nix/prisma-language-server.nix
             ./nix/shell.nix
             ./nix/vscode.nix
           ];
