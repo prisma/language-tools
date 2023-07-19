acb82a9ab9503016093eaa90d4376de1e7a7076f
Joël Galeran <Jolg42@users.noreply.github.com>
2023-07-19 12:27:54 +0200

    fix(vscode): read telemetryLevel too, to decide if telemetry should be enabled

--- packages/vscode/src/telemetryReporter.ts
+++ packages/vscode/src/telemetryReporter.ts
@@ -1,14 +1,22 @@
 import { check } from 'checkpoint-client'
 import { Disposable, workspace } from 'vscode'
 import { getProjectHash } from './hashes'
+
 export default class TelemetryReporter {
   private userOptIn = false
   private readonly configListener: Disposable
 
   private static TELEMETRY_CONFIG_ID = 'telemetry'
-  private static TELEMETRY_CONFIG_ENABLED_ID = 'enableTelemetry'
+  // Deprecated since https://code.visualstudio.com/updates/v1_61#_telemetry-settings
+  private static TELEMETRY_CONFIG_ENABLED_ID_DEPRECATED = 'enableTelemetry'
+  // It is replaced by `telemetryLevel`
+  // https://code.visualstudio.com/docs/getstarted/telemetry
+  private static TELEMETRY_CONFIG_LEVEL_ID = 'telemetryLevel'
 
-  constructor(private extensionId: string, private extensionVersion: string) {
+  constructor(
+    private extensionId: string,
+    private extensionVersion: string,
+  ) {
     this.updateUserOptIn()
     this.configListener = workspace.onDidChangeConfiguration(() => this.updateUserOptIn())
   }
@@ -24,9 +32,17 @@ export default class TelemetryReporter {
   }
 
   private updateUserOptIn() {
-    const config = workspace.getConfiguration(TelemetryReporter.TELEMETRY_CONFIG_ID)
-    if (this.userOptIn !== config.get<boolean>(TelemetryReporter.TELEMETRY_CONFIG_ENABLED_ID, true)) {
-      this.userOptIn = config.get<boolean>(TelemetryReporter.TELEMETRY_CONFIG_ENABLED_ID, true)
+    const telemetryConfig = workspace.getConfiguration(TelemetryReporter.TELEMETRY_CONFIG_ID)
+    // Deprecated since https://code.visualstudio.com/updates/v1_61#_telemetry-settings
+    const isTelemetryEnabled = telemetryConfig.get<boolean>(TelemetryReporter.TELEMETRY_CONFIG_ENABLED_ID_DEPRECATED)
+    const telemetryLevel = telemetryConfig.get<string>(TelemetryReporter.TELEMETRY_CONFIG_LEVEL_ID)
+
+    if (isTelemetryEnabled && telemetryLevel === 'all') {
+      this.userOptIn = true
+      console.info('Telemetry is enabled for Prisma extension')
+    } else {
+      this.userOptIn = false
+      console.info('Telemetry is disabled for Prisma extension')
     }
   }
 
