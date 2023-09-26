f65ab0b60e92f21fa5ac506a18e6c6eb4df7fb31
Sophie Atkins <atkins@prisma.io>
2023-09-26 02:54:14 +0200

    move -> generator - removeInvalidFieldSuggestions
    remove completionUtils

--- packages/language-server/src/lib/completion/generator.ts
+++ packages/language-server/src/lib/completion/generator.ts
@@ -4,7 +4,7 @@ import * as completions from './completions.json'
 import { Block, getValuesInsideSquareBrackets, isInsideAttribute } from '../ast'
 import { convertToCompletionItems } from './internals'
 import { klona } from 'klona'
-import { removeInvalidFieldSuggestions } from './completionUtils'
+
 import listAllAvailablePreviewFeatures from '../prisma-schema-wasm/listAllAvailablePreviewFeatures'
 
 /**
@@ -93,6 +93,36 @@ function handlePreviewFeatures(
   }
 }
 
+/**
+ * Removes all field suggestion that are invalid in this context. E.g. fields that are used already in a block will not be suggested again.
+ * This function removes all field suggestion that are invalid in a certain context. E.g. in a generator block `provider, output, platforms, pinnedPlatForm`
+ * are possible fields. But those fields are only valid suggestions if they haven't been used in this block yet. So in case `provider` has already been used, only
+ * `output, platforms, pinnedPlatform` will be suggested.
+ */
+function removeInvalidFieldSuggestions(
+  supportedFields: string[],
+  block: Block,
+  lines: string[],
+  position: Position,
+): string[] {
+  let reachedStartLine = false
+  for (const [key, item] of lines.entries()) {
+    if (key === block.range.start.line + 1) {
+      reachedStartLine = true
+    }
+    if (!reachedStartLine || key === position.line) {
+      continue
+    }
+    if (key === block.range.end.line) {
+      break
+    }
+    const fieldName = item.replace(/ .*/, '')
+    if (supportedFields.includes(fieldName)) {
+      supportedFields = supportedFields.filter((field) => field !== fieldName)
+    }
+  }
+  return supportedFields
+}
 export function getSuggestionForGeneratorField(block: Block, lines: string[], position: Position): CompletionItem[] {
   // create deep copy
   const suggestions: CompletionItem[] = klona(supportedGeneratorFields)
