a31612e98d57b7a25ce21fd21eaefb01a7ef48c6
Sophie Atkins <atkins@prisma.io>
2023-10-06 15:57:13 +0200

    remove unused

--- packages/language-server/src/lib/ast/index.ts
+++ packages/language-server/src/lib/ast/index.ts
@@ -13,21 +13,3 @@ export function convertDocumentTextToTrimmedLineArray(document: TextDocument): s
     .fill(0)
     .map((_, i) => getCurrentLine(document, i).trim())
 }
-
-// TODO UNUSED
-// export function extractBlockName(line: string): string {
-//   const blockType = extractFirstWord(line)
-//   return line.slice(blockType.length, line.length - 1).trim()
-// }
-
-// export function getAllCompositeTypeNames(lines: string[]): string[] {
-//   const typeNames: string[] = []
-//   for (const line of lines) {
-//     const typeRegex = /^type\s+(\w+)\s+{/gm
-//     const result = typeRegex.exec(line)
-//     if (result && result[1]) {
-//       typeNames.push(result[1])
-//     }
-//   }
-//   return typeNames
-// }
