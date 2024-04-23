9097ed4ca4da4f0a6df1925267fb8100fc907fbb
Sergey Tatarintsev <tatarintsev@prisma.io>
2024-04-23 17:53:29 +0200

    Spawn vscode with shell

--- scripts/e2eTestsOnVsix/install_extension.js
+++ scripts/e2eTestsOnVsix/install_extension.js
@@ -24,6 +24,7 @@ async function installExtension({ extensionType, extensionVersion }) {
     const result = childProcess.spawnSync(cli, ['--install-extension', `${extensionName}@${extensionVersion}`], {
       encoding: 'utf-8',
       stdio: 'pipe',
+      shell: true,
     })
     console.log(result)
     if (result.stderr.includes('Failed')) {
