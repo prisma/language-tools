a161f5f936f14b154b09c6e63c2f1e3e72141d51
Sophie Atkins <atkins@prisma.io>
2024-05-08 11:47:18 +0200

    update guard for notif so we're not constantly updating the setting

--- packages/vscode/src/notifications.ts
+++ packages/vscode/src/notifications.ts
@@ -3,18 +3,18 @@ import { workspace, window, env, Uri, ConfigurationTarget } from 'vscode'
 export const showPDPNotification = async () => {
   const pdpNotif = workspace.getConfiguration('prisma').get('pdpNotification', true)
 
-  if (pdpNotif) {
-    void window
-      .showInformationMessage(
-        'Supercharge your Prisma ORM usage with global database caching, serverless connection pooling and real-time database events.',
-        'Visit Prisma Data Platform',
-      )
-      .then((v) => {
-        if (v === undefined) return
+  if (!pdpNotif) return
 
-        void env.openExternal(Uri.parse('https://pris.ly/cli/pdp'))
-      })
-  }
+  void window
+    .showInformationMessage(
+      'Supercharge your Prisma ORM usage with global database caching, serverless connection pooling and real-time database events.',
+      'Visit Prisma Data Platform',
+    )
+    .then((v) => {
+      if (v === undefined) return
+
+      void env.openExternal(Uri.parse('https://pris.ly/cli/pdp'))
+    })
 
   await workspace.getConfiguration('prisma').update('pdpNotification', false, ConfigurationTarget.Global)
 }
