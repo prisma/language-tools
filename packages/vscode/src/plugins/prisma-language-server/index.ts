48e0775237304d38e1c9047fbc90c6d52cf3a39f
Sophie <29753584+Druue@users.noreply.github.com>
2024-05-13 13:33:50 +0200

    feat(vsc): Add PDP notification (#1716)
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>
    Co-authored-by: Joël Galeran <Jolg42@users.noreply.github.com>

--- packages/vscode/src/plugins/prisma-language-server/index.ts
+++ packages/vscode/src/plugins/prisma-language-server/index.ts
@@ -37,6 +37,7 @@ import { PrismaVSCodePlugin } from '../types'
 import paths from 'env-paths'
 import FileWatcher from 'watcher'
 import { CodelensProvider, generateClient } from '../../CodeLensProvider'
+import { showPDPNotification } from '../../notifications'
 
 const packageJson = require('../../../../package.json') // eslint-disable-line
 
@@ -209,6 +210,8 @@ const plugin: PrismaVSCodePlugin = {
         },
       } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
     }
+
+    await showPDPNotification()
 
     context.subscriptions.push(
       // when the file watcher settings change, we need to ensure they are applied
