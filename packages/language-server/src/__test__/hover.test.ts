4f1c55098aa256abf4cae581ddf57af472926981
Sophie Atkins <atkins@prisma.io>
2024-07-04 21:02:36 +0200

    basic LT hover test-suite

--- packages/language-server/src/__test__/hover.test.ts
+++ packages/language-server/src/__test__/hover.test.ts
@@ -4,7 +4,7 @@ import { getMultifileHelper } from './MultifileHelper'
 import { describe } from 'node:test'
 
 describe('hover', () => {
-  test('model doc', async () => {
+  test('model doc from field', async () => {
     const helper = await getMultifileHelper('user-posts')
     const user = helper.file('User.prisma')
 
@@ -26,7 +26,7 @@ describe('hover', () => {
       }
       \`\`\`
       ___
-      one-to-many 
+      one-to-many
       ___
       This is a blog post",
         },
@@ -34,7 +34,7 @@ describe('hover', () => {
     `)
   })
 
-  test('enum doc', async () => {
+  test('enum doc from field', async () => {
     const helper = await getMultifileHelper('user-posts')
     const user = helper.file('User.prisma')
 
@@ -54,6 +54,81 @@ describe('hover', () => {
       \`\`\`
       ___
       My favourite is the red panda, could you tell?",
+        },
+      }
+    `)
+  })
+
+  test('composite doc from field', async () => {
+    const helper = await getMultifileHelper('user-posts')
+    const user = helper.file('User.prisma')
+
+    const response = handleHoverRequest(helper.schema, user.textDocument, {
+      textDocument: {
+        uri: user.uri,
+      },
+      position: user.lineContaining('address Address').characterAfter('Addr'),
+    })
+
+    expect(response).toMatchInlineSnapshot(`
+      {
+        "contents": {
+          "kind": "markdown",
+          "value": "\`\`\`prisma
+      type Address {}
+      \`\`\`
+      ___
+      Petrichor V",
+        },
+      }
+    `)
+  })
+
+  test('doc from block name', async () => {
+    const helper = await getMultifileHelper('user-posts')
+    const user = helper.file('animal.prisma')
+
+    const response = handleHoverRequest(helper.schema, user.textDocument, {
+      textDocument: {
+        uri: user.uri,
+      },
+      position: user.lineContaining('enum FavouriteAnimal {').characterAfter('Fav'),
+    })
+
+    expect(response).toMatchInlineSnapshot(`
+      {
+        "contents": {
+          "kind": "markdown",
+          "value": "\`\`\`prisma
+      enum FavouriteAnimal {}
+      \`\`\`
+      ___
+      My favourite is the red panda, could you tell?",
+        },
+      }
+    `)
+  })
+
+  test('doc from field name', async () => {
+    const helper = await getMultifileHelper('user-posts')
+    const user = helper.file('address.prisma')
+
+    const response = handleHoverRequest(helper.schema, user.textDocument, {
+      textDocument: {
+        uri: user.uri,
+      },
+      position: user.lineContaining('country String').characterAfter('cou'),
+    })
+
+    expect(response).toMatchInlineSnapshot(`
+      {
+        "contents": {
+          "kind": "markdown",
+          "value": "\`\`\`prisma
+      country
+      \`\`\`
+      ___
+      ISO 3166-2 standard",
         },
       }
     `)
