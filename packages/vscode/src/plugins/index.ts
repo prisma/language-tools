fa7ea0664419784a734aaba495dc54b039853a00
Florian Gößler <florian.goessler@hey.com>
2025-05-06 11:33:27 +0200

    feat: ORM-902 agent tools (#1836)

--- packages/vscode/src/plugins/index.ts
+++ packages/vscode/src/plugins/index.ts
@@ -1,6 +1,7 @@
+import aiTools from './ai-tools'
 import languageServer from './prisma-language-server'
 import { PrismaVSCodePlugin } from './types'
 
-const plugins: PrismaVSCodePlugin[] = [languageServer]
+const plugins: PrismaVSCodePlugin[] = [languageServer, aiTools]
 
 export default plugins
