06c20f8e1041bce4df893ca2dfd99fd889b2bddd
Florian Gößler <florian.goessler@hey.com>
2025-05-15 18:49:39 +0200

    Revert "feat(ppg): ORM-945 add dummy manage ppg ui (#1841)" (#1845)
    48f93df1a1bb3e25b70d72ec29960f4cf0db9201

--- packages/vscode/src/plugins/index.ts
+++ packages/vscode/src/plugins/index.ts
@@ -1,8 +1,7 @@
 import aiTools from './ai-tools'
 import languageServer from './prisma-language-server'
-import ppgManager from './prisma-postgres-manager'
 import { PrismaVSCodePlugin } from './types'
 
-const plugins: PrismaVSCodePlugin[] = [languageServer, aiTools, ppgManager]
+const plugins: PrismaVSCodePlugin[] = [languageServer, aiTools]
 
 export default plugins
