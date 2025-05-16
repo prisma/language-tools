0a115794095969ce4c87dd5bab1ac24f53484a7c
Florian Gößler <florian.goessler@hey.com>
2025-05-16 16:02:36 +0200

    feat: ORM-934 add oauth login (#1843)

--- packages/vscode/src/plugins/prisma-postgres-manager/commands/login.ts
+++ packages/vscode/src/plugins/prisma-postgres-manager/commands/login.ts
@@ -1,10 +1,12 @@
 import { commands } from 'vscode'
 import { PrismaPostgresRepository } from '../PrismaPostgresRepository'
+import { Auth } from '../management-api/auth'
 
-export const login = async (ppgRepository: PrismaPostgresRepository) => {
+export const login = async (ppgRepository: PrismaPostgresRepository, auth: Auth) => {
   if ((await ppgRepository.getWorkspaces()).length === 0) {
     await commands.executeCommand('setContext', 'prisma.initialLoginInProgress', true)
   }
-  await ppgRepository.workspaceLogin()
+  await auth.login()
+  await ppgRepository.workspaceLogin() // TODO: remove this once actual api connected
   await commands.executeCommand('setContext', 'prisma.initialLoginInProgress', false)
 }
