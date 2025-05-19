d4ebbf2187bbdf6569ea692581f1773e3a395ed8
Florian Goessler <florian.goessler@hey.com>
2025-05-19 21:34:55 +0800

    fix: fix glob api changes

--- packages/vscode/src/__test__/index.ts
+++ packages/vscode/src/__test__/index.ts
@@ -1,8 +1,8 @@
 import path from 'path'
 import Mocha from 'mocha'
-import glob from 'glob'
+import { glob } from 'glob'
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -11,28 +11,14 @@ export function run(): Promise<void> {
 
   const testsRoot = __dirname
 
-  return new Promise((resolve, reject) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return reject(err)
-      }
+  const files = await glob('**/**.test.js', { cwd: testsRoot })
+  // Add files to the test suite
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
 
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            reject(new Error(`${failures} tests failed.`))
-          } else {
-            resolve()
-          }
-        })
-      } catch (err) {
-        console.error(err)
-        reject(err)
-      }
-    })
+  // Run the mocha test
+  mocha.run((failures) => {
+    if (failures > 0) {
+      throw new Error(`${failures} tests failed.`)
+    }
   })
 }
