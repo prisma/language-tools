b79db1ed794db787af1f5bfac5bcd6b6ee7ab433
pierre <pierreantoine.urvoy@gmail.com>
2025-06-06 06:29:09 -1000

    Update packages/vscode/src/plugins/prisma-postgres-manager/utils/waitForPortBorrowed.ts
    Co-authored-by: Igal Klebanov <igalklebanov@gmail.com>

--- packages/vscode/src/plugins/prisma-postgres-manager/utils/waitForPortBorrowed.ts
+++ packages/vscode/src/plugins/prisma-postgres-manager/utils/waitForPortBorrowed.ts
@@ -1,13 +1,13 @@
 import { connect } from 'net'
 
-export async function waitForPortBorrowed(port: number, retries = 0): Promise<void> {
-  if (retries >= 10) return
+export async function waitForPortBorrowed(port: number, attemptsMade = 0): Promise<void> {
+  if (attemptsMade >= 10) return
 
-  await new Promise((resolve) => setTimeout(resolve, retries * 100))
+  await new Promise((resolve) => setTimeout(resolve, attemptsMade * 100))
 
   await new Promise<void>((resolve, reject) => {
     const socket = connect(port, '127.0.0.1', resolve)
     socket.once('error', () => (socket.destroy(), reject()))
     socket.once('connect', () => (socket.destroy(), resolve()))
-  }).catch(() => waitForPortBorrowed(port, retries + 1))
+  }).catch(() => waitForPortBorrowed(port, attemptsMade + 1))
 }
