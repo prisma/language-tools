2f11f5d331764817171cc4366f681af89d116375
Pierre-Antoine Urvoy <pierreantoine.urvoy@gmail.com>
2025-06-12 09:51:04 -1000

    add missing refactor

--- packages/vscode/src/plugins/prisma-postgres-manager/commands/copyLocalDatabaseUrl.ts
+++ packages/vscode/src/plugins/prisma-postgres-manager/commands/copyLocalDatabaseUrl.ts
@@ -1,20 +1,14 @@
 import { env, window } from 'vscode'
-import z from 'zod'
-
-const CopyLocalDatabaseUrlArgsSchema = z.object({
-  url: z.string(),
-})
-
-export type CopyLocalDatabaseUrlArgs = z.infer<typeof CopyLocalDatabaseUrlArgsSchema>
+import { LocalDatabase, LocalDatabaseSchema } from '../PrismaPostgresRepository'
 
 export async function copyLocalDatabaseUrl(args: unknown) {
-  const item = CopyLocalDatabaseUrlArgsSchema.parse(args)
+  const item = LocalDatabaseSchema.parse(args)
 
   await env.clipboard.writeText(item.url)
 
   void window.showInformationMessage(`PPg Dev URL copied to your clipboard!`)
 }
 
-export async function copyLocalDatabaseUrlSafely(args: CopyLocalDatabaseUrlArgs) {
+export async function copyLocalDatabaseUrlSafely(args: LocalDatabase) {
   return copyLocalDatabaseUrl(args)
 }
