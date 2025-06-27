afdffbf58a7dcb84db8eb6b8ba78640ff938c5e0
Igal Klebanov <igalklebanov@gmail.com>
2025-06-27 20:45:15 +0300

    Enables telemetry collection for Studio
    Adds telemetry collection for Prisma Studio usage.
    
    The telemetry data includes information about the database connection (local or remote), VS Code environment details, and a client event ID.
    
    This information is used to gain insights into how Studio is used and improve the user experience.

--- packages/vscode/src/plugins/prisma-studio/commands/launch/openNewStudioTab.ts
+++ packages/vscode/src/plugins/prisma-studio/commands/launch/openNewStudioTab.ts
@@ -1,13 +1,29 @@
 import { ExtensionContext, ViewColumn, window } from 'vscode'
 import { getStudioPageHtml } from './getStudioPageHtml'
 import { startStudioServer } from './startStudioServer'
+import TelemetryReporter from '../../../../telemetryReporter'
+import type { LaunchArg } from '../../../prisma-postgres-manager/commands/launchStudio'
+
+export interface OpenNewStudioTabArgs {
+  context: ExtensionContext
+  database: LaunchArg
+  dbUrl: string
+}
 
 /**
  * Opens Prisma Studio in a webview panel.
  * @param args - An object containing the database URL and extension context.
  */
-export async function openNewStudioTab(args: { dbUrl: string; context: ExtensionContext }) {
-  const { server, url } = await startStudioServer(args)
+export async function openNewStudioTab(args: OpenNewStudioTabArgs) {
+  const { context } = args
+  const { extension } = context
+
+  const telemetryReporter = new TelemetryReporter(
+    `prisma.${extension.id}.studio`,
+    (extension.packageJSON as { version: string }).version,
+  )
+
+  const { server, url } = await startStudioServer({ ...args, telemetryReporter })
 
   const panel = window.createWebviewPanel('studio', 'Studio', ViewColumn.One, {
     enableScripts: true,
@@ -19,5 +35,6 @@ export async function openNewStudioTab(args: { dbUrl: string; context: Extension
   panel.onDidDispose(() => {
     server.close()
     console.log(`Studio server has been closed (${url})`)
+    telemetryReporter.dispose()
   })
 }
