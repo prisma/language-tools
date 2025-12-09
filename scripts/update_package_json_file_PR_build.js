926ec8bc6f7ac89a72521f513dff5d5a4fce31d8
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-12-09 15:51:52 +0100

    fix: pnpm lockfile in "update*" scripts

--- scripts/update_package_json_file_PR_build.js
+++ scripts/update_package_json_file_PR_build.js
@@ -1,14 +1,24 @@
+const execa = require('execa')
 const path = require('path')
 const { writeJsonToPackageJson, getPackageJsonContent } = require('./util')
 
 if (require.main === module) {
-  const vscodePackageJsonPath = path.join(__dirname, '../packages/vscode/package.json')
+  ;(async () => {
+    const vscodePackageJsonPath = path.join(__dirname, '../packages/vscode/package.json')
 
-  let content = getPackageJsonContent({ path: vscodePackageJsonPath })
+    let content = getPackageJsonContent({ path: vscodePackageJsonPath })
 
-  // Change name so VS Code doesn't try to auto-update it to the current Insider version
-  content['name'] = 'prisma-insider-pr-build'
-  content['displayName'] = `Prisma - Insider - PR ${process.env.PR_NUMBER} Build - ${new Date().toISOString()}`
+    // Change name so VS Code doesn't try to auto-update it to the current Insider version
+    content['name'] = 'prisma-insider-pr-build'
+    content['displayName'] = `Prisma - Insider - PR ${process.env.PR_NUMBER} Build - ${new Date().toISOString()}`
 
-  writeJsonToPackageJson({ content: content, path: vscodePackageJsonPath })
+    writeJsonToPackageJson({ content: content, path: vscodePackageJsonPath })
+
+    // Update pnpm-lock.yaml after package.json changes
+    console.log('Running pnpm install to update pnpm-lock.yaml...')
+    await execa('pnpm', ['i'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
+  })().catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
 }
--- scripts/update_package_json_files.js
+++ scripts/update_package_json_files.js
@@ -13,14 +13,18 @@ function bumpVersionInVSCodeRepo({ version, name, displayName, description, prev
   writeJsonToPackageJson({ content: content, path: vscodePackageJsonPath })
 }
 
-function bumpLSVersionInExtension({ version }) {
+async function bumpLSVersionInExtension({ version }) {
   const vscodePackageJsonPath = path.join(__dirname, '../packages/vscode/package.json')
   let content = getPackageJsonContent({ path: vscodePackageJsonPath })
   content['dependencies']['@prisma/language-server'] = version
   writeJsonToPackageJson({ content: content, path: vscodePackageJsonPath })
+  
+  // Update pnpm-lock.yaml after package.json changes
+  console.log('Running pnpm install to update pnpm-lock.yaml...')
+  await execa('pnpm', ['i'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
 }
 
-function bumpVersionsInRepo({ channel, newExtensionVersion, newPrismaVersion = '' }) {
+async function bumpVersionsInRepo({ channel, newExtensionVersion, newPrismaVersion = '' }) {
   const languageServerPackageJsonPath = path.join(__dirname, '../packages/language-server/package.json')
   const rootPackageJsonPath = path.join(__dirname, '../package.json')
 
@@ -48,34 +52,32 @@ function bumpVersionsInRepo({ channel, newExtensionVersion, newPrismaVersion = '
 
   // update dependency and engines versions in packages/language-server/package.json
   if (newPrismaVersion !== '') {
-    ;(async () => {
-      // Find the version needed for `@prisma/prisma-schema-wasm`
-      // Let's look into the `package.json` of the `@prisma/engines` package
-      // and get the version of `@prisma/engines-version` it uses
-      const { stdout } = await execa('npm', ['show', `@prisma/engines@${newPrismaVersion}`, 'dependencies', '--json'])
-      console.debug(stdout)
-      const npmInfoOutput = JSON.parse(stdout)
-      const engineVersion = npmInfoOutput['@prisma/engines-version'] // 2.26.0-23.9b816b3aa13cc270074f172f30d6eda8a8ce867d
-      console.debug({ engineVersion })
-      const engineSha = engineVersion.split('.')[3]
-      console.debug({ engineSha })
+    // Find the version needed for `@prisma/prisma-schema-wasm`
+    // Let's look into the `package.json` of the `@prisma/engines` package
+    // and get the version of `@prisma/engines-version` it uses
+    const { stdout } = await execa('npm', ['show', `@prisma/engines@${newPrismaVersion}`, 'dependencies', '--json'])
+    console.debug(stdout)
+    const npmInfoOutput = JSON.parse(stdout)
+    const engineVersion = npmInfoOutput['@prisma/engines-version'] // 2.26.0-23.9b816b3aa13cc270074f172f30d6eda8a8ce867d
+    console.debug({ engineVersion })
+    const engineSha = engineVersion.split('.')[3]
+    console.debug({ engineSha })
 
-      const languageServerPackageJson = getPackageJsonContent({
-        path: languageServerPackageJsonPath,
-      })
-      // update engines sha
-      languageServerPackageJson['prisma']['enginesVersion'] = engineSha
-      // update engines version
-      languageServerPackageJson['dependencies']['@prisma/config'] = newPrismaVersion
-      languageServerPackageJson['dependencies']['@prisma/prisma-schema-wasm'] = engineVersion
-      languageServerPackageJson['dependencies']['@prisma/schema-files-loader'] = newPrismaVersion
-      // update CLI version
-      languageServerPackageJson['prisma']['cliVersion'] = newPrismaVersion
-      writeJsonToPackageJson({
-        content: languageServerPackageJson,
-        path: languageServerPackageJsonPath,
-      })
-    })()
+    const languageServerPackageJson = getPackageJsonContent({
+      path: languageServerPackageJsonPath,
+    })
+    // update engines sha
+    languageServerPackageJson['prisma']['enginesVersion'] = engineSha
+    // update engines version
+    languageServerPackageJson['dependencies']['@prisma/config'] = newPrismaVersion
+    languageServerPackageJson['dependencies']['@prisma/prisma-schema-wasm'] = engineVersion
+    languageServerPackageJson['dependencies']['@prisma/schema-files-loader'] = newPrismaVersion
+    // update CLI version
+    languageServerPackageJson['prisma']['cliVersion'] = newPrismaVersion
+    writeJsonToPackageJson({
+      content: languageServerPackageJson,
+      path: languageServerPackageJsonPath,
+    })
   }
 
   // update version in root package.json
@@ -91,32 +93,41 @@ function bumpVersionsInRepo({ channel, newExtensionVersion, newPrismaVersion = '
   let lspPackageJson = getPackageJsonContent({ path: lsPackageJsonPath })
   lspPackageJson['version'] = newExtensionVersion
   writeJsonToPackageJson({ content: lspPackageJson, path: lsPackageJsonPath })
+
+  // Update pnpm-lock.yaml after package.json changes
+  console.log('Running pnpm install to update pnpm-lock.yaml...')
+  await execa('pnpm', ['i'], { cwd: path.join(__dirname, '..'), stdio: 'inherit' })
 }
 
 module.exports = { bumpVersionsInRepo }
 
 if (require.main === module) {
-  const args = process.argv.slice(2)
-  if (args.length === 3) {
-    console.log('Bumping Prisma CLI version, extension and Language Server version in repo.')
-    bumpVersionsInRepo({
-      channel: args[0],
-      newExtensionVersion: args[1],
-      newPrismaVersion: args[2],
-    })
-  } else if (args.length === 2) {
-    console.log('Bumping extension and Language Server version in repo.')
-    bumpVersionsInRepo({
-      channel: args[0],
-      newExtensionVersion: args[1],
-    })
-  } else if (args.length === 1) {
-    // only bump Language Server version in extension
-    console.log('Bumping Language Server version in extension.')
-    bumpLSVersionInExtension({
-      version: args[0],
-    })
-  } else {
-    throw new Error(`Expected 1, 2 or 3 arguments, but received ${args.length}.`)
-  }
+  ;(async () => {
+    const args = process.argv.slice(2)
+    if (args.length === 3) {
+      console.log('Bumping Prisma CLI version, extension and Language Server version in repo.')
+      await bumpVersionsInRepo({
+        channel: args[0],
+        newExtensionVersion: args[1],
+        newPrismaVersion: args[2],
+      })
+    } else if (args.length === 2) {
+      console.log('Bumping extension and Language Server version in repo.')
+      await bumpVersionsInRepo({
+        channel: args[0],
+        newExtensionVersion: args[1],
+      })
+    } else if (args.length === 1) {
+      // only bump Language Server version in extension
+      console.log('Bumping Language Server version in extension.')
+      await bumpLSVersionInExtension({
+        version: args[0],
+      })
+    } else {
+      throw new Error(`Expected 1, 2 or 3 arguments, but received ${args.length}.`)
+    }
+  })().catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
 }
