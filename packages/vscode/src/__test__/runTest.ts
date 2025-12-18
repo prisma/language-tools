00bd938a815a5c87595d931c97f5ef1e6fad4332
Florian Goessler <goessler@prisma.io>
2025-12-18 14:32:29 +0100

    chore: simplify e2e test script - drop local vs published ls logic

--- packages/vscode/src/__test__/runTest.ts
+++ packages/vscode/src/__test__/runTest.ts
@@ -1,11 +1,11 @@
-import path from 'path'
+import path from 'node:path'
 import { runTests } from '@vscode/test-electron'
 
-// This is executed from dist-tests like `node dist-tests/__test__/runTest true`
+// This is executed from dist-tests like `node dist-tests/__test__/runTest`
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 const packageJson = require('../../package.json') as { engines: { vscode: string } }
 
-function test({ PRISMA_USE_LOCAL_LS, version }: { PRISMA_USE_LOCAL_LS: string; version?: string }) {
+function test(version?: string) {
   // The folder containing the Extension Manifest package.json
   // Passed to `--extensionDevelopmentPath`
   const extensionDevelopmentPath = path.resolve(__dirname, '../../')
@@ -36,35 +36,20 @@ function test({ PRISMA_USE_LOCAL_LS, version }: { PRISMA_USE_LOCAL_LS: string; v
       // ? multiple for one extension. So lets start like this.
       // '--log critical',
     ],
-    extensionTestsEnv: {
-      PRISMA_USE_LOCAL_LS,
-    },
   })
 }
 
 async function main(): Promise<void> {
   try {
-    const args = process.argv.slice(2)
-    // eslint-disable-next-line
-    if (args.length < 1) {
-      console.error('Expected one argument, but received none.')
-      process.exit(1)
-    }
-
     // 1 - Run on our minimum supported version from package.json
     // eslint-disable-next-line
     const minimumSupportedVersion: string = packageJson.engines.vscode.replace('~', '').replace('^', '') // remove semver chars
     console.log(`*** Testing on minimum supported version of VS Code: ${minimumSupportedVersion} ***`)
-    await test({
-      PRISMA_USE_LOCAL_LS: args[0],
-      version: minimumSupportedVersion,
-    })
+    await test(minimumSupportedVersion)
 
     // 2 - Run again on latest version
     console.log(`*** Testing on latest version of VS Code ***`)
-    await test({
-      PRISMA_USE_LOCAL_LS: args[0],
-    })
+    await test()
   } catch (err) {
     const errMsg = err instanceof Error ? ` ${err.message}` : ''
 
--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-USE_LOCAL=${1-false}
-
 # Build extension with esbuild and compile tests with tsc
 echo "Building extension and compiling tests..."
 cd packages/vscode || exit 1
@@ -15,10 +13,4 @@ export CODE_TESTS_PATH
 CODE_TESTS_WORKSPACE="$(pwd)/packages/vscode/fixtures"
 export CODE_TESTS_WORKSPACE
 
-if [ "$USE_LOCAL" = "useLocalLS" ]; then 
-    echo "Using local Language Server."
-    node "$(pwd)/packages/vscode/dist-tests/__test__/runTest" "true"
-else 
-    echo "Using published Language Server."
-    node "$(pwd)/packages/vscode/dist-tests/__test__/runTest" "false"
-fi
+node "$(pwd)/packages/vscode/dist-tests/__test__/runTest"
